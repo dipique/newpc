cf75478665e4c1b88910257006f8dfcbe26e2592
Daniel Kaschel <dkaschel@aiscaregroup.com>
2025-11-20 13:51:00 -0600

    organize to-dos

--- install.ps1
+++ install.ps1
@@ -1,39 +1,3 @@
-# setup:
-#   Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-#   install winget
-#   winget install Microsoft.Powershell # we have a script to do this!
-#   winget install Git.Git
-#   cd \
-#   md dev
-#   cd dev
-#   git clone https://github.com/dipique/newpc.git ./newpc
-
-# todo:
-#   dark mode
-#   enable remote desktop
-#   start menu
-#     remove default apps
-#     remove weather
-#     disable switcher
-#     left -aligned tasks
-#     search bar
-#     add shortcuts -- should be an option for an app, maybe experiment in setup first
-#       everything
-#       vs code
-#       chrome
-#   features
-#   set chrome security features?
-#   lynx
-#     create dev drive
-#     c:\lynx\src, setup
-#     nodep-setup.ps1 -localdb rmt-lt-dkasche3
-#   chrome remote desktop
-#   set machine name
-#   terminal profile
-#   sleep/power profile
-#   vn (optional?)
-#   need to use a loop like setup to detect failures and resume
-
 param(
     [string]$LogDir,
     [string]$CacheDir
---
+++ readme.md
@@ -0,0 +1,12 @@
+# setup
+
+``` powershell
+  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+  install winget
+  winget install Microsoft.Powershell # we have a script to do this!
+  winget install Git.Git
+  cd \
+  md dev
+  cd dev
+  git clone https://github.com/dipique/newpc.git ./newpc
+```
