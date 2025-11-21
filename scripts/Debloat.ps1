d1377a02f8be65a545c6a2aab0c2342534201c42
Daniel Kaschel <dkaschel@aiscaregroup.com>
2025-11-21 11:42:10 -0600

    update debloat script

--- scripts/Debloat.ps1
+++ scripts/Debloat.ps1
@@ -1,52 +1,32 @@
-$bloat_apps = @(
-    "Microsoft.ZuneMusic"
-  , "Microsoft.Music.Preview"
-  , "Microsoft.XboxIdentityProvider"
-  , "Microsoft.BingTravel"
-  , "Microsoft.BingHealthAndFitness"
-  , "Microsoft.BingFoodAndDrink"
-  , "Microsoft.People"
-  , "Microsoft.BingFinance"
-  , "Microsoft.3DBuilder"
-  , "Microsoft.BingNews"
-  , "Microsoft.BingSports"
-  , "Microsoft.Getstarted"
-  , "Microsoft.MicrosoftSolitaireCollection"
-  , "Microsoft.MicrosoftOfficeHub"
-  , "Microsoft.BingWeather"
-  , "Microsoft.GetHelp"
-  , "Microsoft.Messaging"
-  , "Microsoft.News"
-  , "Microsoft.Office.Lens"
-  , "Microsoft.Office.Sway"
-  , "Microsoft.OneConnect"
-  , "Microsoft.Print3D"
-  , "Microsoft.SkypeApp"
-  , "*EclipseManager*"
-  , "*ActiproSoftwareLLC*"
-  , "*AdobeSystemsIncorporated.AdobePhotoshopExpress*"
-  , "*Duolingo-LearnLanguagesforFree*"
-  , "*PandoraMediaInc*"
-  , "*CandyCrush*"
-  , "*BubbleWitch3Saga*"
-  , "*Wunderlist*"
-  , "*Flipboard*"
-  , "*Twitter*"
-  , "*Facebook*"
-  , "*Minecraft*"
-  , "*Royal Revolt*"
-  , "*Sway*"
-  , "*McAfee*"
-  , "*Dropbox*"
-)
+# to get existing apps (for updating the config)
+#   ipmo Appx -UseWindowsPowershell
+#   Get-AppxPackage | Format-Table -Property @{n='Name';e={$_.Name}}, @{n='Version';e={$_.Version}}
+
+
+# Load bloat apps from JSON configuration
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$configPath = Join-Path (Split-Path -Parent $scriptPath) "cfg\debloat.json"
+
+if (Test-Path $configPath) {
+    $config = Get-Content $configPath | ConvertFrom-Json
+    $bloat_apps = $config.apps
+} else {
+    Write-Warning "Configuration file not found: $configPath"
+    exit 1
+}
 
 ipmo Appx -UseWindowsPowershell
-foreach ($bloat_app in $bloat_apps) {
-    if ($bloat_app) {
-        Write-Host "Removing $bloat_app..."
-        $pkg = Get-AppxPackage -name $bloat_app
-        if ($pkg) {
-            $pkg | Remove-AppxPackage
-        }
-    }
-}
+$bloat_apps | % { Write-Host "Removing $_..."; Get-AppxPackage -name $_ } | ? { $_ } | Remove-AppxPackage
+
+# foreach ($bloat_app in $bloat_apps) {
+#     if ($bloat_app) {
+#         Write-Host "Removing $bloat_app..."
+#         $pkg = Get-AppxPackage -name $bloat_app
+#         if ($pkg) {
+#             $pkg | Remove-AppxPackage
+#         }
+#     }
+# }
+
+exit 0
+
--- todo.md
+++ todo.md
@@ -4,3 +4,7 @@ chrome remote desktop
 enable rdp
 need to do that thing for remote desktop
   runas /u:MicrosoftAccount\dipique@gmail.com winver
+debloat
+  move items to config
+  add instructions for detecting existing apps (to remove)
+  update list
